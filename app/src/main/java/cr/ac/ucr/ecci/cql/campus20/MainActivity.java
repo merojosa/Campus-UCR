0e5ac1d26c7e5a1aeae41693f1f4028288e1b846
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2020-04-29 12:11:25 -0600

    Boton para probar la actividad UcrEats

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
@@ -2,7 +2,12 @@ package cr.ac.ucr.ecci.cql.campus20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cr.ac.ucr.ecci.cql.campus20.ucr_eats.MainUcrEats;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Instanciar boton para pasar a la actividad UcrEats
+        Button buttonUcrEats = (Button) findViewById(R.id.buttonUcrEats);
+        // Asocia evento clic al boton
+        buttonUcrEats.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                irUcrEats();
+            }
+        });
+    }
+
+    private void irUcrEats()
+    {
+        // Intent para llamar a la Actividad MainUcrEats
+        Intent intent = new Intent(this, MainUcrEats.class);
+        // Llamada a la actividad
+        startActivity(intent);
     }
 }
--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/ucr_eats/MainUcrEats.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/ucr_eats/MainUcrEats.java
@@ -2,14 +2,18 @@ package cr.ac.ucr.ecci.cql.campus20.ucr_eats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cr.ac.ucr.ecci.cql.campus20.R;
 
 public class MainUcrEats extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ucr_eats);
     }
