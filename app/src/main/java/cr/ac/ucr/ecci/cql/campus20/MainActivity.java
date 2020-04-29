7bb4284d5969b7ccf4b3bd930b5be4ecc5aabc17
Denisse_ <denisse.alfaro@ucr.ac.cr>
2020-04-29 14:39:24 -0600

    Se crea carpeta de red mujeres, activity, layout. Se muestra mapa de prueba con Mapbox.

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
@@ -2,7 +2,12 @@ package cr.ac.ucr.ecci.cql.campus20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cr.ac.ucr.ecci.cql.campus20.red_mujeres.MainRedMujeres;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button); //TODO:Cambiar nombre variable. Botón para ir a mapa
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, MainRedMujeres.class); //Mapa pantalla principal red mujeres
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/MainRedMujeres.java
@@ -0,0 +1,36 @@
+package cr.ac.ucr.ecci.cql.campus20.red_mujeres;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import cr.ac.ucr.ecci.cql.campus20.R;
+
+public class MainRedMujeres extends AppCompatActivity {
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token)); //Tomar el token público
+        setContentView(R.layout.activity_red_mujeres);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() { //Estilo mapa (?)
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+                    }
+                });
+            }
+        });
+    }
+}
