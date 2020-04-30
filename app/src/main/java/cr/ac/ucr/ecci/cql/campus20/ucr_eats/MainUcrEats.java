f9f21a2910d8e3313a7b17bc58430437dd526837
Diego-Orozco <diego.orozco@ucr.ac.cr>
2020-04-30 02:32:04 -0600

    the correct display of the list<string> with the names of the UCR restaurants is started. In addition, the search bar that allows filtering between the restaurant is implemented.

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/ucr_eats/MainUcrEats.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/ucr_eats/MainUcrEats.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,43 +18,93 @@ import cr.ac.ucr.ecci.cql.campus20.R;
 // https://github.com/tutsplus/Android-CardViewRecyclerView
 public class MainUcrEats extends AppCompatActivity
 {
+    private EditText inputSearch;
+    private RVAdapter sodasAdapter;
+
     private List<SodaCard> sodaCards = null;
     private RecyclerView recyclerViewSodas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ucr_eats);
+        getSupportActionBar().hide();
 
-        recyclerViewSodas = (RecyclerView)findViewById(R.id.ucr_eats_rv);
-
-        recyclerViewSodas.setLayoutManager(new LinearLayoutManager(this));
-
-        // Si no se cambia el tamanno, hacer esto mejora el performance
-        recyclerViewSodas.setHasFixedSize(true);
+        setupInputSearch();
+        setupRecyclerView();
 
         obtenerSodas();
         inicializarAdapter();
     }
 
-    /*
-     * Esto creo que es lo de Diego, la cosa es usar la consulta de la base de datos y ojo, muy importante,
-     * la unica informacion que se ocupa es el nombre de la soda y el nombre de la imagen (que tiene que estar en la carpeta
-     * drawable). Por lo tanto, para encapsularlo y evitarnos conflictos, hice una clase SodaCard (que tiene nombre e imagen nada
-     * mas). Entonces, Diego, use esa clase para inicializar la lista sodaCards. El brete suyo me parece que es filtrar esa
-     * info para solamente tener dicho nombre e imagen.
-     */
+    private void setupInputSearch() {
+        this.inputSearch = findViewById(R.id.search_filter);
+        this.inputSearch.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_search_black_24dp, 0, 0, 0);
+        this.inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filtrar(s.toString());
+            }
+        });
+    }
+
+    private void setupRecyclerView() {
+        recyclerViewSodas = (RecyclerView)findViewById(R.id.ucr_eats_rv);
+        recyclerViewSodas.setLayoutManager(new LinearLayoutManager(this));
+
+        // Si no se cambia el tamanno, hacer esto mejora el performance
+        recyclerViewSodas.setHasFixedSize(true);
+    }
+
+    private void filtrar(String texto) {
+
+        ArrayList<SodaCard> filtrarLista = new ArrayList<>();
+
+        for(SodaCard usuario : sodaCards) {
+            if(usuario.getNombre().toLowerCase().contains(texto.toLowerCase())) {
+                filtrarLista.add(usuario);
+            }
+        }
+
+        sodasAdapter.filter(filtrarLista);
+    }
+
     private void obtenerSodas()
     {
+        // initialize the soda list
         sodaCards = new ArrayList<>();
-        // Importante tener el nombre de las imagenes con caracteres alfanumericos y '_' unicamente.
-        sodaCards.add(new SodaCard("Soda La U", R.drawable.la_u));
-        sodaCards.add(new SodaCard("Plaza Chou", R.drawable.plaza_chou));
+        int index = 0;
+        // get soda names
+        List<String> sodaName = obtenerNombreSodas();
+
+        // fill soda list
+        sodaCards.add(new SodaCard(sodaName.get(index++), R.drawable.la_u));
+        sodaCards.add(new SodaCard(sodaName.get(index++), R.drawable.plaza_chou));
+        sodaCards.add(new SodaCard(sodaName.get(index++), R.drawable.la_u));
+    }
+
+    /**
+     * metodo que se espera tenga el retorno de una lista de string con los nombres de las sodas
+     */
+    private List<String> obtenerNombreSodas() {
+        List<String> names = new ArrayList<String>();
+        names.add("Soda La U");
+        names.add("Plaza Chou");
+        names.add("Soda Facultad Derecho");
+
+        return names;
     }
 
     private void inicializarAdapter(){
-        RVAdapter adapter = new RVAdapter(sodaCards);
-        recyclerViewSodas.setAdapter(adapter);
+        this.sodasAdapter = new RVAdapter(sodaCards);
+        recyclerViewSodas.setAdapter(sodasAdapter);
     }
 }
