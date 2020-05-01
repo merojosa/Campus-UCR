25e72fdd500b0a5d06b73a530daa82c13bd2c2b6
John Hernández <johnenderhernandez@gmail.com>
2020-05-01 06:14:35 -0600

    Implementando botones temporales para poder ver la pantalla inicial del foro e ir a los temas

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
@@ -1,8 +1,12 @@
 package cr.ac.ucr.ecci.cql.campus20;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cr.ac.ucr.ecci.cql.campus20.foro_general.MainForoGeneral;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //boton temporal para ir al foro general
+        Button buttonForoGeneral = (Button) findViewById(R.id.buttonForoGeneral);
+
+        // Asocia evento clic al boton
+        buttonForoGeneral.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                irForoGeneral();
+            }
+        });
+    }
+
+    // Ir al foro
+    private void irForoGeneral () {
+        Intent intent = new Intent(this, MainForoGeneral.class);
+        // Llamada a la actividad
+        startActivity(intent);
     }
 }
--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/MainForoGeneral.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/MainForoGeneral.java
@@ -2,7 +2,10 @@ package cr.ac.ucr.ecci.cql.campus20.foro_general;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cr.ac.ucr.ecci.cql.campus20.R;
 
@@ -12,5 +15,24 @@ public class MainForoGeneral extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_foro_general);
+
+
+
+
+        //boton temporal para ir a la pantalla de temas
+        Button buttonTemas = (Button) findViewById(R.id.buttonTemas);
+
+        // Asocia evento clic al boton
+        buttonTemas.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                irATemas();
+            }
+        });
+    }
+    // Ir al foro
+    private void irATemas() {
+        Intent intent = new Intent(this, ForoGeneralVerTemas.class);
+        // Llamada a la actividad
+        startActivity(intent);
     }
 }
