cb97bedd987fbefdb5bd072b9c1301d87d7178b2
JosueZR <josue.zeledonruiz@ucr.ac.cr>
2020-05-01 15:41:58 -0600

    Navegación entre pantalla de inicio y módulo de foro realizada. Falta arreglar detalles de la visualización de las demás opciones de la barra de navegación.

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
@@ -1,10 +1,15 @@
 package cr.ac.ucr.ecci.cql.campus20;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import cr.ac.ucr.ecci.cql.campus20.foro_general.MainForoGeneral;
 
@@ -25,7 +30,36 @@ public class MainActivity extends AppCompatActivity {
                 irForoGeneral();
             }
         });
+
+
+        // Inicialización de la barra de navegación general
+        BottomNavigationView generalNavigationView = findViewById(R.id.bottom_navigation);
+        // Dejar por defecto el Inicio seleccionado
+        generalNavigationView.setSelectedItemId(R.id.inicio);
+        // Ejecución del listener para cambiar de actividad
+        generalNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                // Dependiendo del item seleccionado, ejecutará el Intent del módulo específico
+                switch(menuItem.getItemId()){
+                    case R.id.ucreats:
+                        break;
+                    case R.id.mujeres:
+                        break;
+                    case R.id.foro:
+                        Intent intentForo = new Intent(MainActivity.this, MainForoGeneral.class);
+                        startActivity(intentForo);
+                        break;
+                    case R.id.lugares:
+                        break;
+                }
+                return false;
+            }
+        });
     }
+
+
 
     // Ir al foro
     private void irForoGeneral () {
