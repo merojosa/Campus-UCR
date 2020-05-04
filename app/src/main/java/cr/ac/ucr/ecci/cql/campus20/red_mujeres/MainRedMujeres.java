19362ce17116593a379006006abc2768bf02f3c0
dianaarias <diana.ariasrojas@ucr.ac.cr>
2020-05-03 20:44:23 -0600

    Cambio Maps SDK a Navigation SDK p. futuras funcionalidades(naveg. aun no soportada)

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/MainRedMujeres.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/MainRedMujeres.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.BitmapFactory;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.mapbox.geojson.Feature;
@@ -31,21 +32,45 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacem
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
+// Clases para calcular una ruta
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import android.util.Log;
+
+// classes necesarias para la interfaz de usuario de navegación
+import android.view.View;
+import android.widget.Button;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 import cr.ac.ucr.ecci.cql.campus20.R;
 
-public class MainRedMujeres extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener  {
+public class MainRedMujeres extends AppCompatActivity implements OnMapReadyCallback,  PermissionsListener  {
+
+    //variables para agregar la capa de localización
     private MapView mapView;
     private static final String SOURCE_ID = "SOURCE_ID";
     private static final String ICON_ID = "ICON_ID";
     private static final String LAYER_ID = "LAYER_ID";
-
     private PermissionsManager permissionsManager;
     private MapboxMap mapboxMap;
 
+    //variables para calcular y dibujar una ruta
+    private DirectionsRoute currentRoute;
+    private static final String TAG = "DirectionsActivity";
+    private NavigationMapRoute navigationMapRoute;
+
+    //variables para inicializar navegación
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +98,10 @@ public class MainRedMujeres extends AppCompatActivity implements OnMapReadyCallb
                     enableLocationComponent(style);
                 }
             });
+
+        //Para que la brujula que centra la camara no desaparezca
+        mapboxMap.getUiSettings().setCompassFadeFacingNorth(false);
+
         mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
             @Override
             public boolean onMapClick(@NonNull LatLng point) {
@@ -109,13 +138,117 @@ public class MainRedMujeres extends AppCompatActivity implements OnMapReadyCallb
         });
     }
 
+//    @Override
+//    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+//        this.mapboxMap = mapboxMap;
+//        mapboxMap.setStyle(getString(R.string.navigation_guidance_day), new Style.OnStyleLoaded() {
+//            @Override
+//            public void onStyleLoaded(@NonNull Style style) {
+//                enableLocationComponent(style);
+//
+//                addDestinationIconSymbolLayer(style);
+//
+//                mapboxMap.addOnMapClickListener(MainRedMujeres.this);
+//                button = findViewById(R.id.startButton);
+//                button.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        boolean simulateRoute = true;
+//                        NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+//                                .directionsRoute(currentRoute)
+//                                .shouldSimulateRoute(simulateRoute)
+//                                .build();
+//                        // Call this method with Context from within an Activity
+//                        NavigationLauncher.startNavigation(MainRedMujeres.this, options);
+//                    }
+//                });
+//            }
+//
+//        });
+//    }
+
+    //*************************************************
+    //NAVEGACION:
+    //*************************************************
+    private void addDestinationIconSymbolLayer(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addImage("destination-icon-id",
+                BitmapFactory.decodeResource(this.getResources(), R.drawable.mapbox_marker_icon_default));
+        GeoJsonSource geoJsonSource = new GeoJsonSource("destination-source-id");
+        loadedMapStyle.addSource(geoJsonSource);
+        SymbolLayer destinationSymbolLayer = new SymbolLayer("destination-symbol-layer-id", "destination-source-id");
+        destinationSymbolLayer.withProperties(
+                iconImage("destination-icon-id"),
+                iconAllowOverlap(true),
+                iconIgnorePlacement(true)
+        );
+        loadedMapStyle.addLayer(destinationSymbolLayer);
+    }
+
+//    @SuppressWarnings( {"MissingPermission"})
+//    @Override
+//    public boolean onMapClick(@NonNull LatLng point) {
+//
+//        //LocationComponent locationComponent = null;
+//        Point destinationPoint = Point.fromLngLat(point.getLongitude(), point.getLatitude());
+//        Point originPoint = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+//                locationComponent.getLastKnownLocation().getLatitude());
+//
+//        GeoJsonSource source = mapboxMap.getStyle().getSourceAs("destination-source-id");
+//        if (source != null) {
+//            source.setGeoJson(Feature.fromGeometry(destinationPoint));
+//        }
+//
+//        getRoute(originPoint, destinationPoint);
+//        button.setEnabled(true);
+//        button.setBackgroundResource(R.color.mapBoxBlue);
+//        return true;
+//    }
+
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(this)
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        // You can get the generic HTTP info about the response
+                        Log.d(TAG, "Response code: " + response.code());
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Log.e(TAG, "No routes found");
+                            return;
+                        }
+
+                        currentRoute = response.body().routes().get(0);
+
+                        // Draw the route on the map
+                        if (navigationMapRoute != null) {
+                            //navigationMapRoute.removeRoute(); //Deprecated
+                            navigationMapRoute.updateRouteVisibilityTo(false);
+                            navigationMapRoute.updateRouteArrowVisibilityTo(false);
+                        } else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView, mapboxMap, R.style.NavigationMapRoute);
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                        Log.e(TAG, "Error: " + throwable.getMessage());
+                    }
+                });
+    }
+
 
     @SuppressWarnings( {"MissingPermission"})
     private void enableLocationComponent(@NonNull Style loadedMapStyle) {
     // Check if permissions are enabled and if not request
         if (PermissionsManager.areLocationPermissionsGranted(this))
         {
-
             // Get an instance of the component
             LocationComponent locationComponent = mapboxMap.getLocationComponent();
 
@@ -154,19 +287,23 @@ public class MainRedMujeres extends AppCompatActivity implements OnMapReadyCallb
     @Override
     public void onPermissionResult(boolean granted)
     {
+//        if (granted) {
+//            enableLocationComponent(mapboxMap.getStyle(new Style.OnStyleLoaded()
+//            {
+//                @Override
+//                public void onStyleLoaded(@NonNull Style style) {
+//                    enableLocationComponent(style);
+//                }
+//            });
         if (granted) {
-            mapboxMap.getStyle(new Style.OnStyleLoaded()
-            {
-                @Override
-                public void onStyleLoaded(@NonNull Style style) {
-                    enableLocationComponent(style);
-                }
-            });
+            enableLocationComponent(mapboxMap.getStyle());
         } else {
             Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
             finish();
         }
     }
+
+
 
     @Override
     @SuppressWarnings( {"MissingPermission"})
