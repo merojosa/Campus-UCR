fe72277a5043746c3b0bb083eef05365adf2f161
hernantorres <hernan.torres@ucr.ac.cr>
2020-05-05 09:08:49 -0600

    Eliminado boton para ir a actividad.

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/MainActivity.java
@@ -40,13 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 goInterestPoints();
             }
         });
-
-        findViewById(R.id.ucreatsbtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                irUcrEats();
-            }
-        });
     }
 
     private void goInterestPoints() {
