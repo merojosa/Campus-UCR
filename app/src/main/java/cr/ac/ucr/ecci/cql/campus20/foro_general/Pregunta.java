5d6d9bccdf8b3e1f4c0458e2dc01caa3f92ae074
NeoCR27 <urenaesteban27@gmail.com>
2020-05-18 20:08:41 -0600

    Cambio para que clase Pregunta tenga un ArrayList de objetos Respuesta. Además, esqueleto de clase Respuesta

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/Pregunta.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/Pregunta.java
@@ -1,9 +1,11 @@
 package cr.ac.ucr.ecci.cql.campus20.foro_general;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Pregunta {
+    private int id;
     private Temas tema;
     private String pregunta;
-    private String[] respuestas;
+    private ArrayList<Respuesta> respuestas;
 }
---
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/Respuesta.java
@@ -0,0 +1,6 @@
+package cr.ac.ucr.ecci.cql.campus20.foro_general;
+
+class Respuesta {
+    private int id;
+    private String respuesta;
+}
