376628d0964be9f6afaddc1a93739106da82e8b1
Berta <berta.sanchez@ucr.ac.cr>
2020-05-19 12:38:54 -0600

    Popup (Material Dialog) para validacion creado.

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/MenuRedMujeres.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/MenuRedMujeres.java
@@ -1,15 +1,52 @@
 package cr.ac.ucr.ecci.cql.campus20.red_mujeres;
 import cr.ac.ucr.ecci.cql.campus20.R;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 
 public class MenuRedMujeres extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_red_mujeres);
+
+        // create a dialog with AlertDialog builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(MenuRedMujeres.this, R.style.AppTheme_RedMujeres);
+        builder.setTitle("Hoki");
+        builder.setMessage("dfkef");
+
+        String positiveText = getString(android.R.string.ok);
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // dismiss alert dialog, update preferences with game score and restart play fragment
+                        //presenter.updateGameScore(gameScore);
+                        //Log.d("myTag", "positive button clicked");
+                        dialog.dismiss();
+                    }
+                });
+
+        String negativeText = getString(android.R.string.cancel);
+        builder.setNegativeButton(negativeText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // dismiss dialog, start counter again
+                        dialog.dismiss();
+                        //Log.d("myTag", "negative button clicked");
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
