7360d7b2fcce24da84eb267e40d8eefb96fe683a
NeoCR27 <urenaesteban27@gmail.com>
2020-05-19 12:53:49 -0600

    Cambios en el onCreate de la base que se llama únicamente si la base no existe, además, la pobla con 3 temas de prueba

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/ForoGeneralDatabase.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/ForoGeneralDatabase.java
@@ -1,10 +1,13 @@
 package cr.ac.ucr.ecci.cql.campus20.foro_general;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,27 +20,59 @@ import cr.ac.ucr.ecci.cql.campus20.foro_general.models.Tema;
 @Database(entities = {Tema.class, Pregunta.class}, version = 3, exportSchema = false)
 public abstract class ForoGeneralDatabase extends RoomDatabase
 {
-    public abstract PreguntaDao preguntaDao();
-    public abstract TemaDao temaDao();
+    private static ForoGeneralDatabase INSTANCE;
 
-    private static ForoGeneralDatabase INSTANCE = null;
 
-    private static final int NUMBER_OF_THREADS = 4;
-    public static final ExecutorService databaseWriteExecutor =
-            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
-
-    public static ForoGeneralDatabase getDatabase(final Context context)
-    {
-        if(INSTANCE == null)
-        {
+    public static ForoGeneralDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
             synchronized (ForoGeneralDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            ForoGeneralDatabase.class, "ForoGeneral-database").fallbackToDestructiveMigration()
+                            ForoGeneralDatabase.class, "ForoGeneral-Database")
+                            .allowMainThreadQueries() // SHOULD NOT BE USED IN PRODUCTION !!!
+                            .addCallback(new RoomDatabase.Callback() {
+                                @Override
+                                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                                    super.onCreate(db);
+                                    new PopulateDbAsync(INSTANCE).execute();
+                                }
+                            })
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    public void clearDb() {
+        if (INSTANCE != null) {
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    }
+
+    public abstract TemaDao temaDao();
+
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+        private final TemaDao temaDao;
+
+        public PopulateDbAsync(ForoGeneralDatabase instance) {
+            temaDao = instance.temaDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            temaDao.borrarTodo();
+
+            Tema temaUno = new Tema(0, "General", 10);
+            Tema temaDos = new Tema(0, "Info", 3);
+            Tema temaTres = new Tema(0, "Becas", 5);
+
+            temaDao.insert(temaUno);
+            temaDao.insert(temaDos);
+            temaDao.insert(temaTres);
+
+            return null;
+        }
     }
 }
