611fc483170d6209c7fd12a7a330e2618d604fac
FelipeAlmanza <FelipeAlmanza@users.noreply.github.com>
2020-05-23 23:30:22 -0600

    Modificacion de entidad respuesta y creacion inicial de su dao.

---
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/Daos/RespuestaDao.java
@@ -0,0 +1,37 @@
+package cr.ac.ucr.ecci.cql.campus20.foro_general.Daos;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import cr.ac.ucr.ecci.cql.campus20.foro_general.models.Respuesta;
+
+@Dao
+public interface RespuestaDao {
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void insert(Respuesta respuesta);
+
+    @Update(onConflict = OnConflictStrategy.ABORT)
+    void update(Respuesta respuesta);
+
+    @Delete
+    void delete(Respuesta respuesta);
+
+    @Query("SELECT * FROM Respuesta WHERE id = :id")
+    Respuesta getRespuestaPorID(int id);
+
+    @Query("SELECT * FROM Respuesta WHERE preguntaID = :preguntaID")
+    LiveData<List<Respuesta>> getRespuestasDePregunta(int preguntaID);
+
+    @Query("DELETE FROM Respuesta")
+    void borrarTodo();
+
+    @Query("DELETE FROM Respuesta WHERE id = :id")
+    void borrarRespuesta(int id);
+}
