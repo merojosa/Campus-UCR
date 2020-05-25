45a9a87b1c255a7719eb127a29c995855da65727
andresnboza <andresnboza92@gmail.com>
2020-05-24 18:56:28 -0600

    ruteo completo

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/InterestPoints/Mapbox/Map.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/InterestPoints/Mapbox/Map.java
@@ -1,116 +1,102 @@
 package cr.ac.ucr.ecci.cql.campus20.InterestPoints.Mapbox;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.TextView;
-
-import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.Point;
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.camera.CameraPosition;
-import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.plugins.markerview.MarkerView;
-import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager;
-
-import java.security.interfaces.DSAKeyPairGenerator;
-import java.util.ArrayList;
-import java.util.List;
-
 import cr.ac.ucr.ecci.cql.campus20.InterestPoints.CoffeShop.CoffeViewActivity;
 import cr.ac.ucr.ecci.cql.campus20.InterestPoints.FacultiesAndSchools.SchoolViewActivity;
 import cr.ac.ucr.ecci.cql.campus20.InterestPoints.IPModel.Coordinate;
 import cr.ac.ucr.ecci.cql.campus20.InterestPoints.IPModel.School;
 import cr.ac.ucr.ecci.cql.campus20.R;
 
-public class Map extends AppCompatActivity implements OnMapReadyCallback{
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.TextView;
+import android.widget.Toast;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+
+// Clases para calcular una ruta
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import android.util.Log;
+
+// classes necesarias para la interfaz de usuario de navegación
+import android.view.View;
+import android.widget.Button;
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cr.ac.ucr.ecci.cql.campus20.R;
+
+public class Map extends AppCompatActivity implements OnMapReadyCallback, MapboxMap.OnMapClickListener,  PermissionsListener  {
+
+    //variables para agregar la capa de localización
     private MapView mapView;
-
+    private PermissionsManager permissionsManager;
+    private LocationComponent locationComponent;
     private static final String SOURCE_ID = "SOURCE_ID";
+    private static final String ICON_ID = "ICON_ID";
+    private static final String LAYER_ID = "LAYER_ID";
+    private MapboxMap mapboxMap;
 
-    TextView textViewName;
-    TextView textViewNameBody;
-    TextView textViewDescriptionKey;
-    TextView textViewDescriptionValue;
-    TextView textViewScheduleKey;
-    TextView textViewScheduleValue;
-    TextView textViewPhoneKey;
-    TextView textViewPhoneValue;
-    TextView textViewFacebook;
-    TextView textViewTwitter;
-    TextView textViewInstragram;
-    TextView textViewWebPage;
-    private double latitude;
-    private double longitude;
-    private String name;
-    private int type;
-    Map mapboxMap;
+    //variables para calcular y dibujar una ruta
+    private DirectionsRoute currentRoute;
+    private static final String TAG = "DirectionsActivity";
+    private NavigationMapRoute navigationMapRoute;
+    private TextView  TextButtton;
+
+    //variables para inicializar navegación
+    private Button button;
+
     School place;
     Coordinate coordinate;
     private Intent details;
-
-
-    // Setting the start location of the map view
-    private MapboxMapOptions setStartLocation(double latitude, double longitude) {
-        MapboxMapOptions options = MapboxMapOptions.createFromAttributes(this, null)
-                .camera(new CameraPosition.Builder()
-                        .target(new LatLng(latitude, longitude))
-                        .zoom(18)
-                        .build());
-        return options;
-    }
-
-    // Basic mapbox configurations
-    private void settingConfigurations() {
-        // Setting mapbox access token - talking to API
-        Mapbox.getInstance(this, getString(R.string.MAPBOX_ACCESS_TOKEN));
-        // Union with the view of map
-        setContentView(R.layout.activity_map);
-    }
-
-    // Creating the map
-    private void mapCreation(MapboxMapOptions options, Bundle savedInstanceState) {
-        mapView = new MapView(this, options);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
-
-                mapboxMap.setStyle(Style.OUTDOORS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
-                    }
-                });
-
-                MarkerOptions options = new MarkerOptions();
-                options.position(new LatLng(coordinate.getLatitude(), coordinate.getLongitude()));
-                mapboxMap.addMarker(options);
-            }
-        });
-        setContentView(mapView);
-    }
+    Double originLatitude = 0.00;
+    Double originLongitude = 0.00;
+    Double destinationLatitude = 0.00;
+    Double destinationLongitude = 0.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token)); //Tomar el token público
 
-        // Configuration management
-        settingConfigurations();
         // Relationship with the map view
         Intent intentItemList = getIntent();
         // Getting item title information
@@ -119,60 +105,196 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback{
         this.place = getIntent().getParcelableExtra("place");
         // Getting the Coordinates of the place
         this.coordinate = getIntent().getParcelableExtra("coordinate");
-        // Setting the start location of the map view
-        MapboxMapOptions options = setStartLocation(coordinate.getLatitude(), coordinate.getLongitude());
+        destinationLatitude = coordinate.getLatitude();
+        destinationLongitude = coordinate.getLongitude();
 
-        // --------------------
+
         if(getSupportActionBar() != null){
             getSupportActionBar().setTitle(itemTitle);
             getSupportActionBar().show();
         }
 
-        // create map - FINAL STEP
-        mapCreation(options, savedInstanceState);
-    }
-
-    private void settingPlaceInfoOnMarker() {
-
-    }
-
-    private void settingNames() {
-
-        textViewName =          (TextView)findViewById(R.id.nameKey);
-        textViewNameBody =      (TextView)findViewById(R.id.nameValue);
-
-        //textViewDescriptionKey =    (TextView)findViewById(R.id.descriptionKey);
-        //textViewDescriptionValue =  (TextView)findViewById(R.id.descriptionValue);
-        textViewScheduleKey =       (TextView)findViewById(R.id.scheduleKey);
-        textViewScheduleValue =     (TextView)findViewById(R.id.scheduleValue);
-        textViewPhoneKey =          (TextView)findViewById(R.id.phoneKey);
-        textViewPhoneValue =        (TextView)findViewById(R.id.phoneValue);
-
-        /*textViewFacebook =          (TextView)findViewById(R.id.facebook);
-        textViewTwitter =           (TextView)findViewById(R.id.twitter);
-        textViewInstragram =        (TextView)findViewById(R.id.instagram);
-        textViewWebPage =           (TextView)findViewById(R.id.website);*/
-
-
-        textViewName.setText(R.string.placeNameKey);
-        textViewNameBody.setText(place.getName());
-        //textViewDescriptionKey.setText(R.string.placeDescriptionKey);
-        //textViewDescriptionValue.setText(place.getDescription());
-        textViewScheduleKey.setText(R.string.placeScheduleKey);
-        textViewScheduleValue.setText("7:00 am -- 7:00 pm");
-        textViewPhoneKey.setText("Teléfono");
-        textViewPhoneValue.setText("2511 4000");
-
-        // Setting the coordinates
-
+        setContentView(R.layout.activity_map);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
     }
 
     @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        Map.this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                //Estilo cargado y mapa está listo
+                enableLocationComponent(style);
+
+                addDestinationIconSymbolLayer(style);
+
+                mapboxMap.addOnMapClickListener(Map.this);
+                button = findViewById(R.id.startButton);
+
+                //Texto de boton de comienzo de ruteo
+                TextButtton = (TextView)findViewById(R.id.startButton);
+                TextButtton.setText("Ir a " + place.getName());
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean simulateRoute = false;
+                        NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                                .directionsRoute(currentRoute)
+                                .shouldSimulateRoute(simulateRoute)
+                                .build();
+                        // Call this method with Context from within an Activity
+                        NavigationLauncher.startNavigation(Map.this, options);
+                    }
+                });
+
+                setNavigation();
+            }
+
+        });
+    }
+
+
+    //*************************************************
+    //NAVEGACION:
+    //*************************************************
+    private void addDestinationIconSymbolLayer(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addImage("destination-icon-id",
+                BitmapFactory.decodeResource(this.getResources(), R.drawable.mapbox_marker_icon_default));
+        GeoJsonSource geoJsonSource = new GeoJsonSource("destination-source-id");
+        loadedMapStyle.addSource(geoJsonSource);
+        SymbolLayer destinationSymbolLayer = new SymbolLayer("destination-symbol-layer-id", "destination-source-id");
+        destinationSymbolLayer.withProperties(
+                iconImage("destination-icon-id"),
+                iconAllowOverlap(true),
+                iconIgnorePlacement(true)
+        );
+        loadedMapStyle.addLayer(destinationSymbolLayer);
+    }
+
+    public void setNavigation() {
+        Point destinationPoint = Point.fromLngLat(destinationLongitude, destinationLatitude);
+        Point originPoint = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                locationComponent.getLastKnownLocation().getLatitude());
+
+        GeoJsonSource source = mapboxMap.getStyle().getSourceAs("destination-source-id");
+        if (source != null) {
+            source.setGeoJson(Feature.fromGeometry(destinationPoint));
+        }
+
+        getRoute(originPoint, destinationPoint);
+        button.setEnabled(true);
+        button.setBackgroundResource(R.color.mapboxBlue);
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    @Override
+    public boolean onMapClick(@NonNull LatLng point) {
+        return true;
+    }
+
+    private void getRoute(Point origin, Point destination) {
+        NavigationRoute.builder(this)
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(new Callback<DirectionsResponse>() {
+                    @Override
+                    public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                        // You can get the generic HTTP info about the response
+                        Log.d(TAG, "Response code: " + response.code());
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+                            return;
+                        } else if (response.body().routes().size() < 1) {
+                            Log.e(TAG, "No routes found");
+                            return;
+                        }
+
+                        currentRoute = response.body().routes().get(0);
+
+                        // Draw the route on the map
+                        if (navigationMapRoute != null) {
+                            //navigationMapRoute.removeRoute(); //Deprecated
+                            navigationMapRoute.updateRouteVisibilityTo(false);
+                            navigationMapRoute.updateRouteArrowVisibilityTo(false);
+                        } else {
+                            navigationMapRoute = new NavigationMapRoute(null, mapView, mapboxMap, R.style.NavigationMapRoute);
+                        }
+                        navigationMapRoute.addRoute(currentRoute);
+                    }
+
+                    @Override
+                    public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+                        Log.e(TAG, "Error: " + throwable.getMessage());
+                    }
+                });
+    }
+
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this))
+        {
+            // Get an instance of the component
+            locationComponent = mapboxMap.getLocationComponent();
+
+            // Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
+
+            // Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+            // Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        }
+        else
+        {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain)
+    {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted)
+    {
+        if (granted) {
+            enableLocationComponent(mapboxMap.getStyle());
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+
+    @Override
+    @SuppressWarnings( {"MissingPermission"})
     protected void onStart() {
         super.onStart();
         mapView.onStart();
     }
-
 
     @Override
     protected void onResume() {
@@ -199,15 +321,15 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback{
     }
 
     @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 
     @Override
@@ -215,7 +337,6 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback{
 
         Intent intent = getIntent();
         int tipo = intent.getExtras().getInt("typeActivity");
-
 
         getMenuInflater().inflate(R.menu.go_ip_details_menu, menu);
 
@@ -237,10 +358,5 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback{
         });
 
         return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public void onMapReady(@NonNull MapboxMap mapboxMap) {
-
     }
 }
