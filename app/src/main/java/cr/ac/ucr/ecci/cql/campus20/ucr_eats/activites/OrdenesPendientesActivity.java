a4a5c1a6bfc62ce8f128fdd8732ec53be02d5647
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2020-06-03 22:34:33 -0600

    Ícono para poder ir a la actividad de órdenes pendientes

---
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/ucr_eats/activites/OrdenesPendientesActivity.java
@@ -0,0 +1,18 @@
+package cr.ac.ucr.ecci.cql.campus20.ucr_eats.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cr.ac.ucr.ecci.cql.campus20.LoginBD;
+import cr.ac.ucr.ecci.cql.campus20.R;
+
+public class OrdenesPendientesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordenes_pendientes);
+    }
+}
