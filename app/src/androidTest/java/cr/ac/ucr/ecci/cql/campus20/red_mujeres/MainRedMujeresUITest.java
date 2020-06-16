34f5f5f24380d55f111235ec98a0f91c9c0b3698
Berta <berta.sanchez@ucr.ac.cr>
2020-06-16 08:26:46 -0600

    Clase para pruebas de UI

--- app/src/androidTest/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/MainRedMujeresUITest.java
+++ app/src/androidTest/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/MainRedMujeresUITest.java
@@ -1,9 +1,7 @@
 package cr.ac.ucr.ecci.cql.campus20.red_mujeres;
 
 import org.junit.Test;
-
 import cr.ac.ucr.ecci.cql.campus20.R;
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -12,14 +10,14 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-class MainRedMujeresTest {
+class MainRedMujeresUITest {
 
     @Test
     public void testBotonPanico(){
         onView(withId(R.id.sos)).perform(click());
         onView(withId(R.id.sos)).check(matches(withText("¡EMERGENCIA!")));
 
-       // intended(hasComponent(CrearPreguntaForoGeneral.class.getName()));
+        // intended(hasComponent(CrearPreguntaForoGeneral.class.getName()));
     }
 
 }
