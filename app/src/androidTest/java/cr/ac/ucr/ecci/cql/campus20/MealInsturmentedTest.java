9a9d3f2bfe12b925658a7e058f1b0d7cfc27a356
Steven Fernández <victor.fernandezespinoza@ucr.ac.cr>
2020-06-15 21:33:34 -0600

    Instrumented test for parcelable meal

---
+++ app/src/androidTest/java/cr/ac/ucr/ecci/cql/campus20/MealInsturmentedTest.java
@@ -0,0 +1,30 @@
+package cr.ac.ucr.ecci.cql.campus20;
+
+import android.os.Parcel;
+
+import org.junit.Test;
+
+import cr.ac.ucr.ecci.cql.campus20.ucr_eats.models.Meal;
+
+import static org.junit.Assert.assertEquals;
+
+public class MealInsturmentedTest
+{
+    @Test
+    // Reference: https://medium.com/@dbottillo/android-junit-parcelable-model-test-37a2f2ae18b1
+    public void testParcelableName()
+    {
+        //String name, String photo, int type, int price
+        Meal meal = new Meal(0, "Mock", "mock photo", Meal.BREAKFAST, 1500);
+
+        Parcel parcel = Parcel.obtain();
+        meal.writeToParcel(parcel, meal.describeContents());
+
+        parcel.setDataPosition(0);
+
+        Meal parcelMeal = Meal.CREATOR.createFromParcel(parcel);
+
+        assertEquals(meal.getName(), parcelMeal.getName());
+
+    }
+}
