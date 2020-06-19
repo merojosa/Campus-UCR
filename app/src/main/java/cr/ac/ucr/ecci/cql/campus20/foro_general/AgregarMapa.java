bae386ae605da600eab643a220c053616a05b7ae
John Hernández <johnenderhernandez@gmail.com>
2020-06-19 11:22:55 -0600

    CI0161-270 Utilizacion de mapas. Pedir permisos. D:Johnn N:Felipe

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/AgregarMapa.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/foro_general/AgregarMapa.java
@@ -2,27 +2,39 @@ package cr.ac.ucr.ecci.cql.campus20.foro_general;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
+import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.maps.SupportMapFragment;
+import com.mapbox.services.android.navigation.ui.v5.listeners.NavigationListener;
+
+import java.util.List;
 
 import cr.ac.ucr.ecci.cql.campus20.R;
 
-public class AgregarMapa extends AppCompatActivity {
+//MapboxMap.OnMapClickListener,
+public class AgregarMapa extends AppCompatActivity implements PermissionsListener {
 
+    private MapboxMap mapboxMap;
     double lat;
     double lon;
     private PermissionsManager permissionsManager;
@@ -40,6 +52,7 @@ public class AgregarMapa extends AppCompatActivity {
         // Mapbox access token is configured here. This needs to be called either in your application
         // object or in the same activity which contains the mapview.
         Mapbox.getInstance(this, getString(R.string.MAPBOX_ACCESS_TOKEN));
+
 
         // Create supportMapFragment
         SupportMapFragment mapFragment;
@@ -70,11 +83,14 @@ public class AgregarMapa extends AppCompatActivity {
             mapFragment.getMapAsync(new OnMapReadyCallback() {
                 @Override
                 public void onMapReady(@NonNull MapboxMap mapboxMap) {
-                    mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    AgregarMapa.this.mapboxMap = mapboxMap;
+                    AgregarMapa.this.mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                         @Override
                         public void onStyleLoaded(@NonNull Style style) {
                             // Map is set up and the style has loaded. Now you can add data or make other map adjustments
 
+
+                            habilitarPermisos(style);
                             FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionRespuestas);
                             floatingActionButton.setOnClickListener(new View.OnClickListener() {
                                 @Override
@@ -97,7 +113,55 @@ public class AgregarMapa extends AppCompatActivity {
     }
 
     //Habilitar permisos de ubicación
-    private void habilitarPermisos(){
+    @SuppressWarnings( {"MissingPermission"})
+    private void habilitarPermisos(@NonNull Style loadedMapStyle){
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this))
+        {
+            // Get an instance of the component
+            locationComponent = mapboxMap.getLocationComponent();
 
+            // Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
+
+            // Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+            // Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        }
+        else
+        {
+            permissionsManager = new PermissionsManager((PermissionsListener) this);
+            permissionsManager.requestLocationPermissions(this);
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain)
+    {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted)
+    {
+        if (granted) {
+            habilitarPermisos(mapboxMap.getStyle());
+        } else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
 }
