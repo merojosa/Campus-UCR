6d92f1f791a6a63360e14eaf6b58230d25469d57
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2020-06-19 11:34:32 -0600

    Arreglo de errores de compilación

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/ucr_eats/MessagingService.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/ucr_eats/MessagingService.java
@@ -1,19 +1,16 @@
 package cr.ac.ucr.ecci.cql.campus20.ucr_eats;
 
-//import FirebaseMessagingService;
-
-import android.util.Log;
-import android.widget.Toast;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MessagingService extends FirebaseMessagingService {
 
     @Override
-    public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
+    public void onNewToken(String token)
+    {
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        sendRegistrationToServer(token);
+        System.out.println("PRUEBA");
     }
 }
