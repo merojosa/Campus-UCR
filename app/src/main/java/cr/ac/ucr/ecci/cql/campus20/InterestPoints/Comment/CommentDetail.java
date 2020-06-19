fa5a1b005ebd483683584851eab62d55129ca9c8
LuisC <luisda7cr@gmail.com>
2020-06-19 10:22:50 -0600

    MPS4-01 Detalle de comentario
    Implementación parcial del detalle de comentario con su view.
    D: Luis Carvajal
    N: Sebastián Cruz

---
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/InterestPoints/Comment/CommentDetail.java
@@ -0,0 +1,17 @@
+package cr.ac.ucr.ecci.cql.campus20.InterestPoints.Comment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cr.ac.ucr.ecci.cql.campus20.R;
+
+public class CommentDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_detail);
+
+    }
+}
