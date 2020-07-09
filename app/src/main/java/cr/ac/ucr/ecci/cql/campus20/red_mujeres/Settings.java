f900aa80a59900b749d615e80a166bdf16928ace
Berta <berta.sanchez@ucr.ac.cr>
2020-07-08 21:23:28 -0600

    Config de Nombre y Numero de Confianza

--- app/src/main/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/Settings.java
+++ app/src/main/java/cr/ac/ucr/ecci/cql/campus20/red_mujeres/Settings.java
@@ -1,9 +1,15 @@
 package cr.ac.ucr.ecci.cql.campus20.red_mujeres;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import cr.ac.ucr.ecci.cql.campus20.R;
@@ -28,6 +34,45 @@ public class Settings extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+            EditTextPreference contactoNom = findPreference("contactoNom");
+            if (contactoNom != null) {
+                contactoNom.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
+                    @Override
+                    public CharSequence provideSummary(EditTextPreference preference) {
+                        String text = preference.getText();
+                        if (TextUtils.isEmpty(text)) {
+                            return "No establecido";
+                        }
+                        return text;
+                    }
+                });
+                contactoNom.setDialogTitle("Establecer Nombre");
+            }
+
+            EditTextPreference contactoNum = findPreference("contactoNum");
+            if (contactoNum != null) {
+                contactoNum.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                            @Override
+                            public void onBindEditText(@NonNull EditText editText) {
+                                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                            }
+                });
+
+                contactoNum.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
+                    @Override
+                    public CharSequence provideSummary(EditTextPreference preference) {
+                        String text = preference.getText();
+                        if (TextUtils.isEmpty(text)) {
+                            return "No establecido";
+                        }
+                        return text;
+                    }
+                });
+                contactoNum.setDialogTitle("Establecer Número");
+            }
+
         }
     }
 }
